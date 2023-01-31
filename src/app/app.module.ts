b9ff69099b85a5e2be6707b741d3a438a1572127
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-31 14:10:17 +0100

    fix: delete pokemon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { ProfilePage } from './pages/profile/profile.page';
-import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 @NgModule({
-  declarations: [AppComponent, LoginPage, ProfilePage, PokemonCataloguePage],
+  declarations: [AppComponent, LoginPage, ProfilePage],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
