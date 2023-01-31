494401eae2f8a40cd268ce25f4ad6d93d327948b
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-31 15:50:32 +0100

    feat: add login functions and redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { ProfilePage } from './pages/profile/profile.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login',
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'pokemons',
+    component: PokemonCataloguePage,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfilePage,
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
