52075ecf92b22e707ebfd135262f38cf417f0eb8
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2023-01-31 14:11:42 +0100

    feat: add pokemon-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { ProfilePage } from './pages/profile/profile.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 @NgModule({
-  declarations: [AppComponent, LoginPage, ProfilePage],
+  declarations: [AppComponent, LoginPage, ProfilePage, PokemonCataloguePage],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
