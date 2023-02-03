db92c7c41a4d3eb3949ba0dbc3f286df56c95579
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-02-03 10:47:58 +0100

    feat: put pokeball on mouse back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,40 +7,40 @@ import { Component, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemon';
-  // mouseMove = false;
-  // constructor(private el: ElementRef) {}
+  mouseMove = false;
+  constructor(private el: ElementRef) {}
 
-  // get tooltip() {
-  //   return this.el.nativeElement.querySelector('.sites-circle');
-  // }
+  get tooltip() {
+    return this.el.nativeElement.querySelector('.sites-circle');
+  }
 
-  // enter() {
-  //   this.tooltip.classList.add('show');
-  // }
-  // mouseStopped() {
-  //   this.mouseMove = false;
+  enter() {
+    this.tooltip.classList.add('show');
+  }
+  mouseStopped() {
+    this.mouseMove = false;
 
-  //   // this.tooltip.classList.add('onMove');
-  //   // the actual function that is called
-  //   // console.log('mousemove: ' + this.mouseMove);
-  // }
-  // move(e: { pageX: number; pageY: number }) {
-  //   this.mouseMove = true;
-  //   // console.log('mousemove: ' + this.mouseMove);
+    // this.tooltip.classList.add('onMove');
+    // the actual function that is called
+    // console.log('mousemove: ' + this.mouseMove);
+  }
+  move(e: { pageX: number; pageY: number }) {
+    this.mouseMove = true;
+    // console.log('mousemove: ' + this.mouseMove);
 
-  //   //  this.tooltip.classList.add('onMove');
+    //  this.tooltip.classList.add('onMove');
 
-  //   var timer;
-  //   clearTimeout(timer);
-  //   timer = setTimeout(this.mouseStopped, 300);
+    var timer;
+    clearTimeout(timer);
+    timer = setTimeout(this.mouseStopped, 300);
 
-  //   const tooltipStyle = this.tooltip.style;
+    const tooltipStyle = this.tooltip.style;
 
-  //   tooltipStyle.left = e.pageX + 'px';
-  //   tooltipStyle.top = e.pageY + 'px';
-  // }
+    tooltipStyle.left = e.pageX + 'px';
+    tooltipStyle.top = e.pageY + 'px';
+  }
 
-  // leave() {
-  //   this.tooltip.classList.remove('show');
-  // }
+  leave() {
+    this.tooltip.classList.remove('show');
+  }
 }
