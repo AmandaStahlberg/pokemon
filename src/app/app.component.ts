6cbbfb3722ecfb22981b4a8c4e59645b9690b087
Amanda Ståhlberg <stahlberg.amanda@hotmail.com>
2023-02-06 09:01:30 +0100

    feat: remove pokeball from mouse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,40 +7,4 @@ import { Component, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemon';
-  mouseMove = false;
-  constructor(private el: ElementRef) {}
-
-  get tooltip() {
-    return this.el.nativeElement.querySelector('.sites-circle');
-  }
-
-  enter() {
-    this.tooltip.classList.add('show');
-  }
-  mouseStopped() {
-    this.mouseMove = false;
-
-    // this.tooltip.classList.add('onMove');
-    // the actual function that is called
-    // console.log('mousemove: ' + this.mouseMove);
-  }
-  move(e: { pageX: number; pageY: number }) {
-    this.mouseMove = true;
-    // console.log('mousemove: ' + this.mouseMove);
-
-    //  this.tooltip.classList.add('onMove');
-
-    var timer;
-    clearTimeout(timer);
-    timer = setTimeout(this.mouseStopped, 300);
-
-    const tooltipStyle = this.tooltip.style;
-
-    tooltipStyle.left = e.pageX + 'px';
-    tooltipStyle.top = e.pageY + 'px';
-  }
-
-  leave() {
-    this.tooltip.classList.remove('show');
-  }
 }
